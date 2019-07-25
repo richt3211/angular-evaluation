ce2357fe717df87f8d434470b501d69c167ab836
delashum <delashum@gmail.com>
2019-07-25 12:29:27 -0600

    server done

---
+++ server/data-service.js
@@ -0,0 +1,14 @@
+const people = require('./PEOPLE.json');
+const cards = require('./CARDS.json');
+
+exports.getPeople = () => [...people];
+exports.getCards = () => [...cards];
+exports.getPerson = id => people.find(e => e.id === id);
+exports.getCard = id => cards.find(e => e.id === id);
+exports.getPersonCards = person => cards.filter(e => e.person_id === person);
+exports.paginatePeople = (offset = 0, num = 100) => people.slice(offset, offset + num);
+exports.addCard = (person_id, card_number) => {
+  const newCard = { person_id, card_number, balance: 0, id: +(Math.random() * 100000).toFixed(0) };
+  cards.push(newCard);
+  return newCard;
+};
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const db = require('./data-service');
+const bp = require('body-parser');
+try {
+  app.use(bp.json());
+
+  app.post('/person/list', (req, res) => {
+    res.json(db.getPeople());
+  });
+
+  app.post('/person/get', (req, res) => {
+    if (req.body != null && req.body.id != null) res.json(db.getPerson(req.body.id));
+    else res.json(null);
+  });
+
+  app.post('/card/query', (req, res) => {
+    if (req.body != null && req.body.person != null) res.json(db.getPersonCards(req.body.person));
+    else res.json([]);
+  });
+
+  app.post('/card/get', (req, res) => {
+    if (req.body != null && req.body.id != null) res.json(db.getCard(req.body.id));
+    else res.json(null);
+  });
+
+  app.post('/card/add', (req, res) => {
+    if (req.body != null && req.body.person_id != null && req.body.card_number)
+      res.json(db.addCard(req.body.person_id, req.body.card_number));
+    else res.json(null);
+  });
+
+  app.listen(3000, () => console.log('server listening on port 3000'));
+} catch (err) {
+  console.error(err);
+}
